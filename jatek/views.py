3e98549aaa2ff0ac8d5714b508098804a3b1d303
B.Dávid <you@example.com>
2026-03-24 09:15:14 +0100

    api endpoint for all questions

--- jatek/views.py
+++ jatek/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Question
+from .serializers import QuestionSerializer
 
-# Create your views here.
+@api_view(["GET"])
+def questions(request):
+    questions = Question.objects.all()
+    serialized = QuestionSerializer(questions, many = True)
+    return Response(serialized.data)
